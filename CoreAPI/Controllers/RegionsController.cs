9b2f8b5ee1588471ef3d6658882065fee8178aad
Madn <madan.patakota@gmail.com>
2024-07-16 10:39:49 +0530

    DTOs

--- CoreAPI/Controllers/RegionsController.cs
+++ CoreAPI/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using CoreAPI.Data;
 using CoreAPI.Domain;
+using CoreAPI.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -31,65 +32,196 @@ namespace CoreAPI.Controllers
 
         // i am going to prepare the one method i.e. Get
 
+
+        
         [HttpGet]
-        public IActionResult GetAllRegions()
+        public async Task<IActionResult> GetAllRegions()
         {
 
-           var regions =  proWalksDbContext.Regions.ToList();
-            
-            ////var regions = new List<Region>
-            //{
-            //    new Region()
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        Area = 2000000,
-            //        Lat = 19.099999,
-            //        Long = 20.0000,
-            //        Name = "Hindu",
-            //        Code = "Hn",
-            //        population = 100000
-            //    },
+            //  3 records
+            var regions = await proWalksDbContext.Regions.ToListAsync();
 
-            //    new Region()
-            //    {
-            //        Id = Guid.NewGuid(),
-            //        Area = 1000000,
-            //        Lat = 9.099999,
-            //        Long = 20.0010,
-            //        Name = "Sikkus",
-            //        Code = "Si",
-            //        population = 2000
-            //    }
-            //};
-            return Ok(regions);
+
+            var regionDtos = new List<RegionDTO>();
+
+            foreach (var region in regions)
+            {
+                regionDtos.Add(new RegionDTO()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Area = region.Area,
+                    population = region.population
+                });
+            }
+            return Ok(regionDtos);
         }
 
 
 
         // I want to fetch the Religion based on the ID
-
-
         //https://localhost/7078/api/Region/{regionid}
+
         [HttpGet]
         [Route("{regionId:Guid}")]
-        public IActionResult GetRegionById([FromRoute] Guid regionId)
+        public async Task<IActionResult> GetRegionById([FromRoute] Guid regionId)
         {
 
             //var id = regionId;
 
+            //var regions =  proWalksDbContext
+             //   .Regions.Find(regionId);
 
-            var regions = proWalksDbContext
-                .Regions.Find(regionId);
+            var regions = await proWalksDbContext
+                .Regions.FindAsync(regionId);
 
-            if(regions == null)
+
+            if (regions == null)
             {
                 return BadRequest();
             }
 
-            
-            return Ok(regions);
+
+            var regionDto = new RegionDTO();
+
+            regionDto.Id = regions.Id;
+            regionDto.Code = regions.Code;
+            regionDto.Name = regions.Name;
+            regionDto.Lat = regionDto.Lat;
+            regionDto.Lat = regions.Lat;
+            regionDto.population = regions.population;
+            regionDto.Area = regions.Area;
+
+
+
+
+
+
+            return Ok(regionDto);
         }
 
+
+        // 1. Real time 
+        // you will receive the region information from clinet who has requested to you
+        // Front end
+        // Insert | Create
+
+        //informaiton like customeid cusotmerlocation abhiid id , jdate 
+
+        //public Iaction
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRegion([FromBody] AddRegionDto addRegionDto)
+        {
+
+            var regionDomainModel = new Region
+            {
+                Code = addRegionDto.Code,
+                Name = addRegionDto.Name,
+                Lat = addRegionDto.Lat,
+                Long = addRegionDto.Long,
+                Area = addRegionDto.Area,
+                population = addRegionDto.population
+            };
+
+            await proWalksDbContext.Regions.AddAsync(regionDomainModel);
+            proWalksDbContext.SaveChanges();
+
+
+            //Map domain model back to the DTO
+
+            //return Ok("no i wou't the response");
+
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name
+            };
+
+            // regionDto.Id = regionDomainModel.Id;
+            /// Prepare the code..
+
+            return Ok(regionDto);
+
+        }
+
+
+
+        // Update ---> you will update the records based on the key(Id)
+
+
+        [HttpPut]
+        [Route("{regionId:Guid}")]
+        //6FDBC8DC-2B84-4747-F588-08DCA54EDBAF
+        // Code : "Siks" , Area : 1000 , Name : "SN" , Lat : 100.09
+        public async Task<IActionResult> updateRegion(
+             [FromRoute] Guid regionId,
+             [FromBody]  UpdateRegionDto updateRegionDto)
+        {
+
+            var regionDomainModel = await proWalksDbContext.Regions.FindAsync(regionId);
+
+            if(regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //no need the add.
+
+            regionDomainModel.Code = updateRegionDto.Code;  // Siks
+            regionDomainModel.population = updateRegionDto.population;
+            regionDomainModel.Area = updateRegionDto.Area;
+            regionDomainModel.Lat = updateRegionDto.Lat;
+            regionDomainModel.Long = updateRegionDto.Long;
+            regionDomainModel.Name = updateRegionDto.Name;
+
+            proWalksDbContext.SaveChanges();
+
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name
+            };
+
+            return Ok(regionDto);
+
+        }
+
+        //On which key(ID) you are going to delete
+        [HttpDelete]
+        [Route("{regionId:Guid}")]
+        public async Task<IActionResult> DeleteRegion([FromRoute] Guid regionId)
+        {
+
+            var regionDomainModel = await proWalksDbContext.Regions.FindAsync(regionId);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //  proWalksDbContext.Regions.Add
+
+            proWalksDbContext.Regions.Remove(regionDomainModel);
+            proWalksDbContext.SaveChanges();
+
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name
+            };
+
+            return Ok(regionDto);
+
+
+
+        }
 
 
 
---
+++ CoreAPI/DTOs/UpdateRegionDto.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreAPI.DTOs
+{
+    public class UpdateRegionDto
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Long { get; set; }
+
+        public long population { get; set; }
+
+
+
+    }
+}
